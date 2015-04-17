f6899c07cdc2985ef0893b82a3921d8054352a9a
Ukov Dmitry <dukov@mirantis.com>
2015-04-17 18:36:38 +0300

    Added copyright
    Change-Id: Ia597c83b2d7ea193e4a15b7ad15071f5553f87a4

--- deployment_scripts/midonetapi_site.pp
+++ deployment_scripts/midonetapi_site.pp
@@ -1,3 +1,17 @@
+#    Copyright 2013 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 $fuel_settings = parseyaml($astute_settings_yaml)
 $nodes_hash = $::fuel_settings['nodes']
 $primary_controller_nodes = filter_nodes($nodes_hash,'role','primary-controller')
@@ -14,4 +28,3 @@ class {'plugin_midonet::repos':
 
 class {'plugin_midonet::midonetapi':
 }
-
--- deployment_scripts/midonetdb_site.pp
+++ deployment_scripts/midonetdb_site.pp
@@ -1,3 +1,17 @@
+#    Copyright 2013 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 $fuel_settings = parseyaml($astute_settings_yaml)
 $nodes_hash = $::fuel_settings['nodes']
 $node = filter_nodes($nodes_hash,'name',$::hostname)
@@ -22,4 +36,3 @@ class {'plugin_midonet::zookeeper':
 class {'plugin_midonet::cassandra':
     stage => cassandra,
 }
-
