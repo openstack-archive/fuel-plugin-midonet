94509830da7ca25f8fc5c0c7d3c197359cc3e4b0
Jaume Devesa <devvesa@gmail.com>
2016-03-16 11:36:31 +0100

    [WIP] Introduce Fuel 8 development
    Fuel 8 adaptation to new `deployment_tasks.yaml`
    
    Change-Id: I729bbc3b9b96899f0fb18ec45e1debab0783e409

--- deployment_scripts/puppet/manifests/midonet-override-hiera.pp
+++ deployment_scripts/puppet/manifests/midonet-override-hiera.pp
@@ -11,13 +11,9 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-notice('MODULAR: midonet-enable-ip.forward.pp')
+notice('MODULAR: midonet-override-hiera.pp')
 
-sysctl::value { 'net.ipv4.ip_forward':
-  value => '1'
-} ->
-
-exec { 'load-sysctl':
-  command => "/sbin/sysctl -p /etc/sysctl.conf",
-  refreshonly => true
+file {'/etc/hiera/plugins/midonet-fuel-plugin.yaml':
+    ensure => file,
+    source => '/etc/fuel/plugins/midonet-fuel-plugin-4.0/puppet/files/midonet-fuel-plugin.yaml'
 }
