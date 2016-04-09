98b053b3e3cc51c709aae096c26b20c242d22719
Samir Ibradžić <sibradzic@gmail.com>
2016-04-09 21:13:25 +0900

    Remove unnecessary exec tasks from BGP GW manifest
    Two task, 'set down external bridge' and 'remove bridge ip address'
    are not really necessary, and they break external network
    connectivity from GW node by removing default gateway
    
    Change-Id: Ib3813747615d4b1bd8a1ca7fd36ba87d58f211f0

--- deployment_scripts/puppet/manifests/midonet-gateway-bgp.pp
+++ deployment_scripts/puppet/manifests/midonet-gateway-bgp.pp
@@ -17,16 +17,9 @@ $bgp_subnet = split($midonet_settings['bgp_cidr'], '/')
 $bgp_subnet_ip = $bgp_subnet[0]
 $bgp_subnet_cidr = $bgp_subnet[1]
 
-exec {"set down external bridge":
- path    => "/usr/bin:/usr/sbin:/sbin",
-  command => "ip link set dev br-ex down"
-} ->
-
-exec {"remove bridge ip address":
-  path    => "/usr/bin:/usr/sbin:/sbin",
-  command => "ip a del $::ipaddress_br_ex dev br-ex",
-  onlyif  => "ip -4 a | /bin/grep br-ex"
-} ->
+notify {"peers":
+   message => "floating neeet si $remote_peers"
+}
 
 exec {"add veth interface":
   path    => "/usr/bin:/usr/sbin:/sbin",
