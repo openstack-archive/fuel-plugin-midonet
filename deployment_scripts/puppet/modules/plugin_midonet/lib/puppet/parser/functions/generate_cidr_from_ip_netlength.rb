3a9c593ca1cd9f0eef701f9e9d41acf434b66672
Alex Ruiz Estradera <ahharu@ahharu.com>
2016-09-09 15:09:24 +0200

    Introduce Fuel 9 and Midonet 5.x
    All changes needed to be compatible with
    Fuel 9 and Midonet 5.x
    
    Change-Id: I9040e5452a37b451b01ba79f56b0a9e98fa726ad

--- deployment_scripts/puppet/modules/plugin_midonet/lib/puppet/parser/functions/generate_cidr_from_ip_netlength.rb
+++ deployment_scripts/puppet/modules/plugin_midonet/lib/puppet/parser/functions/generate_cidr_from_ip_netlength.rb
@@ -1,4 +1,4 @@
-#    Copyright 2015 Midokura SARL.
+#    Copyright 2015 Midokura SARL, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -11,12 +11,15 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-notice('MODULAR: midonet-replace-guess-func.pp')
 
-# NOTE: This replacement may be only needed on Ubuntu hosts
-file_line { 'replace_guess':
-  path     => '/usr/share/neutron-common/plugin_guess_func',
-  match    => '"neutron.plugins.midonet.plugin.MidonetPluginV2"',
-  line     => "\t\"midonet.neutron.plugin_v2.MidonetPluginV2\")",
-  multiple => true
-}
+require 'netaddr'
+
+module Puppet::Parser::Functions
+  newfunction(:generate_cidr_from_ip_netlength, :type => :rvalue, :doc => <<-EOS
+    This function returns BGP cidr CSV as an array
+    EOS
+  ) do |argv|
+    result = NetAddr::CIDR.create(argv[0]).to_s
+    return result
+  end
+end
